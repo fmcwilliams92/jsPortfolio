78cfd06a9c60d0e56ce1762bcd3d4fae5e795823
Fredric McWilliams <fredmcwilliams92@gmail.com>
2024-03-06 22:55:39 -0600

    adding in comments on js functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,20 +2,23 @@
 var headerName = document.getElementById("name");
 headerName.style.color = "black";
 
-// links
+// functions to show you clicked on the social media links
+
+/* github link */
 var gitHub = document.getElementById("gitHub");
 gitHub.addEventListener("click", function() {
   alert("Github button has been clicked")
 });
 
-
+/* linkedIn link */
 var linkedIn = document.getElementById("linkedIn");
 linkedIn.addEventListener("click", function() {
   alert("LinkedIn button has been clicked")
 });
 
+/*instagram link */
 var instagram = document.getElementById("instagram");
 instagram.addEventListener("click", function() {
   alert("Instagram button has been clicked")
-  console.log(instagram, "this has been clicked")
+  console.log(instagram, "booooootcamp")
 });
