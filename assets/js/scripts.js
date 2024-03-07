70af53eac21c14a99fb8ce67e676390511287b25
Fredric McWilliams <fredmcwilliams92@gmail.com>
2024-03-06 21:49:59 -0600

    confirmed link to javascript file and also setup onClick functions for the social media links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,19 @@ var headerName = document.getElementById("name");
 headerName.style.color = "black";
 
 // links
+var gitHub = document.getElementById("gitHub");
+gitHub.addEventListener("click", function() {
+  alert("Github button has been clicked")
+});
+
+
+var linkedIn = document.getElementById("linkedIn");
+linkedIn.addEventListener("click", function() {
+  alert("LinkedIn button has been clicked")
+});
+
+var instagram = document.getElementById("instagram");
+instagram.addEventListener("click", function() {
+  alert("Instagram button has been clicked")
+  console.log(instagram, "this has been clicked")
+});
